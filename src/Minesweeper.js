0882601f08044fae0c949c3898a646968a796303
semihcakir18 <semihcakir04@gmail.com>
2024-08-28 14:20:14 +0300

    -board randomize edildi -flag'lendikten sonra acilan hücrelerin gorunumu duzenlendi -mayinlarin acilma sekli degistirildi

--- src/Minesweeper.js
+++ src/Minesweeper.js
@@ -4,6 +4,7 @@ import Mine from "./mine.png";
 const Minesweeper = () => {
   const rowSize = 10;
   const colSize = 8;
+  const numberOfBombs = 10;
   const directions = [
     [-1, -1],
     [-1, 0],
@@ -35,19 +36,24 @@ const Minesweeper = () => {
     });
     return bombCount;
   };
+  var bombLocations = [];
+  const bombPlacer = (board) => {
+    const newRow = Math.floor(Math.random() * board.length);
+    const newCol = Math.floor(Math.random() * board[0].length);
+    if (board[newRow][newCol] === "bomb") {
+      bombPlacer(board);
+    }
+    if (board[newRow][newCol] === 0) {
+      board[newRow][newCol] = "bomb";
+      bombLocations.push([newRow, newCol]);
+    }
+  };
 
-  const board = [
-    [0, 0, "bomb", 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    ["bomb", 0, 0, 0, 0, "bomb", 0, 0],
-    [0, "bomb", 0, 0, 0, 0, 0, 0],
-    [0, 0, "bomb", 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, "bomb"],
-    [0, 0, 0, 0, 0, "bomb", 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, "bomb"],
-    [0, 0, "bomb", 0, 0, 0, 0, "bomb"],
-    [0, 0, 0, 0, 0, "bomb", 0, "bomb"],
-  ];
+  const board = new Array(rowSize).fill().map(() => new Array(colSize).fill(0));
+
+  for (let i = 0; i < numberOfBombs; i++) {
+    bombPlacer(board);
+  }
 
   const updateBoardWithBombCounts = (board) => {
     return board.map((row, rowIndex) =>
@@ -104,11 +110,26 @@ const Minesweeper = () => {
       revealCounter();
 
       if (updatedBoard[row][col] === "bomb") {
+        if (cell.classList.contains("flagged")) {
+          cell.classList.remove("flagged");
+        }
         cell.innerHTML = `<img src="${Mine}" alt="mine" />`;
+        bombLocations.forEach(([dx, dy], index) => {
+          setTimeout(() => {
+            const bombCell = document.getElementById(`${dx}-${dy}`);
+            if (bombCell.classList.contains("flagged")) {
+              bombCell.classList.remove("flagged");
+            }
+            bombCell.innerHTML = `<img src="${Mine}" alt="mine" />`;
+          }, index * 300);
+        });
         alert("Bombaya bastın!");
       } else {
         cell.textContent = updatedBoard[row][col];
         if (updatedBoard[row][col] === 0) {
+          if (cell.classList.contains("flagged")) {
+            cell.classList.remove("flagged");
+          }
           cell.classList.add("revealed");
           cell.classList.add("green");
           revealCounter();
